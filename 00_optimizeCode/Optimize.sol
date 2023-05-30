b293191f311e64944d74a0edf159cbd271fd9cd9
kihiuFrank <franklinekihiu@gmail.com>
2023-05-30 11:47:47 +0300

    optimized

--- 00_optimizeCode/Optimize.sol
+++ 00_optimizeCode/Optimize.sol
@@ -5,12 +5,12 @@ contract optimize {
     function setCompleted() public pure returns (uint256) {
         uint256 a;
         uint256 b;
-        uint256 c;
+        // uint256 c;
         uint256 d;
-        uint256 x;
-        c = a * b;
-        x = a;
-        d = x * b + 4;
+        // uint256 x;
+        // c = a * b;
+        // x = a;
+        d = a * b + 4;
         return d;
     }
 }
